8a8b7a48378568b3a4a97f02c15cbdf288b0f9ae
ink yuta <country7777iy@gmail.com>
2024-02-04 23:08:58 +0900

    追加：メイン画面html追加

---
+++ php_version/resources/views/layouts/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'/>
+    <title Barcode DB for plants identification />
+    <link rel='stylesheet' href='/css/Common_header.css'>
+    @yield('append_header')
+  </head>
+
+  <body>
+    @yield('headerContent')
+    @yield('main')
+    @yield('scripts')
+  </body>
+</html>
