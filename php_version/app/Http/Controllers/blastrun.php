b6823302d862bd5bfed60fb33485b517d3044fc2
ink yuta <country7777iy@gmail.com>
2024-02-10 23:24:38 +0900

    追加：blast実行、読み出し関数

--- php_version/app/Http/Controllers/blastrun.php
+++ php_version/app/Http/Controllers/blastrun.php
@@ -42,4 +42,93 @@ class blastrun extends Controller
         $query = $_POST['query'];//get sequences user inputs
         file_put_contents($file,$query,LOCK_EX);
     }
+
+    /*
+        Run the Blast application on the server and return the result.
+    */
+    static function blastRun(int $randomNum){
+        $inFileName=  app_path()."../{$randomNum}.fasta";
+        $outResultFile = $randomNum.'_result.txt';
+        $db         = $_POST['db'];
+        $gene       = $_POST['gene'];
+        $blastDb    = app_path().'..'."public/resources/{$gene}/db/{$db}/all" ;
+        $outFmtOpt  = '"6 std qlen slen"' ;
+        $maxTagSeq  = 100;
+
+        $cmd = "blastn -query {$inFileName}  -db {$blastDb} -out {$outResultFile} -outfmt {$outFmtOpt} -max_target_seqs {$maxTagSeq}";
+        exec($cmd, $output);
+        return $outResultFile;
+    }
+
+    static function readResultFile($outputFile){
+        $counter = 0; //ファイルの一行目のみ別処理  
+        $perIdent = [
+            'PRSTRAT' => 0, //トップヒットした参照配列のアライメント開始位置（単位は％)
+            'PREND'=>0      //トップヒットした参照配列のアライメント終了位置（単位は％）
+        ];
+        $objArrayIn_=[];//2 dimentions array. Each item equals to the 1 blast result row.
+        $resultFile = fopen($outputFile,"r"); // The file Blast application output.
+        if(!$resultFile){
+                echo("The file doesn't exist.");
+                return false;
+        }else{
+            while($line = fgets($resultFile)){
+                echo $line;
+                $counter++;
+
+                $_ = explode($line,'\t');
+                $ref        =$_[1];// 参照(reference)のID        
+                $identity   =$_[2];// アライメントした配列長における一致率
+                $alignLen   =$_[3];// アライメント長
+                $missMatch  =$_[4];// ミスマッチのカウント
+                $gapOpen    =$_[5];// ギャップが生じた箇所のカウント
+                $qstart     =$_[6];// クエリのアライメント開始位置
+                $qend       =$_[7];// クエリのアライメント終了位置
+                $rstart     =$_[8];// 参照のアライメント開始位置
+                $rend       =$_[9];// 参照のアライメント終了位置
+                $eval       =$_[10];// E-value
+                $bitScore   =$_[11];// スコア（大きい方が2つの配列は類似していると言える）
+
+                if(counter==1){
+                    
+                    $maxSeqs = 1035;//The max length of the gene seq is 1035
+                    $displayStart= (rstart/$maxSeqs)*100 + '%';//the start positon of refseq to display in SVG file
+
+                    $displayEnd = '';//the End positon of refseq to display in SVG file
+                    $_ = (rend/$maxSeqs)+100;
+                    if($_ >100){
+                        $displayEnd='100%';
+                    }{
+                        $displayEnd= "{$_}%";
+                    }
+                    $perIdent['PRSTRAT']= $displayStart;
+                    $perIdent['PREND']=$displayEnd;
+                }else{
+                    array_push($objArrayIn_,[
+                            URL=>"https://www.ncbi.nlm.nih.gov/protein/{$ref}", // 検索結果に表示するURL
+                            REF=>$ref       ,     // 参照配列(reference)のID
+                            SPE=>'' ,     // 種名
+                            IDEN=>$identity ,     // アライメントした配列長における一致率
+                            ALEN=>$alignLen ,     // アライメント長
+                            MM=>$missMatch  ,     // ミスマッチの総数
+                            GA=>$gapOpen    ,     // ギャップが生じた箇所の総数
+                            QS=>$qstart     ,     // クエリのアライメント開始位置
+                            QE=>$qend       ,     // クエリのアライメント終了位置
+                            RS=>$rstart     ,     // 参照のアライメント開始位置
+                            RE=>$rend       ,     // 参照のアライメント終了位置
+                            EV=>$eval       ,     // E-value値
+                            BS=>$bitScore   ,     // ビットスコア（大きい方が2つの配列は類似していると言える）
+                        ]
+                    );
+                }
+
+            }
+        }
+
+        //after reading file
+        return [
+            perIdent=>perIdent_,
+            objArrayIn=>objArrayIn_
+        ];
+    }
 }
